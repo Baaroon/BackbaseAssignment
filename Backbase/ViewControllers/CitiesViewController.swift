86217565592cfca2b082db91c111851a1101a3d9
Zahra Aghajani <aghajani@gmail.com>
2018-10-16 08:16:07 +0330

    Project initialization
    - Storyboards and ViewControllers are initialized
    - Source JSON file is added

---
+++ Backbase/ViewControllers/CitiesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesViewController.swift
+//  Backbase
+//
+//  Created by Zahra Aghajani on 10/16/18.
+//  Copyright © 2018 Zahra Aghajani. All rights reserved.
+//
+
+import UIKit
+
+class CitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Backbase/ViewControllers/LocationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LocationViewController.swift
+//  Backbase
+//
+//  Created by Zahra Aghajani on 10/16/18.
+//  Copyright © 2018 Zahra Aghajani. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
